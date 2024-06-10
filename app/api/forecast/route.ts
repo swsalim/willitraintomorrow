1f45d2f655225a51ba6b8fa44e188a0581b9f0cd
Yuyu <swsalim@gmail.com>
2024-06-10 13:32:03 +0800

    protect api route

--- app/api/forecast/route.ts
+++ app/api/forecast/route.ts
@@ -4,6 +4,16 @@ export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
   try {
+    const allowedReferrer = new URL(process.env.NEXT_PUBLIC_BASE_URL!).origin
+    const requestReferrer = request.headers.get('referrer')
+
+    if (!requestReferrer || requestReferrer !== allowedReferrer) {
+      return NextResponse.json(
+        { success: false, error: 'Unauthorized request.' },
+        { status: 403 }
+      )
+    }
+
     const requestUrl = new URL(request.url)
     const city = requestUrl.searchParams.get('city')
     const country = requestUrl.searchParams.get('country')
@@ -12,24 +22,23 @@ export async function GET(request: NextRequest) {
     const weatherResponse = await fetch(
       `http://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_WEATHER_API_KEY}&q=${query}&days=2`
     )
-    const weatherData = await weatherResponse.json()
 
-    delete weatherData.forecast?.forecastday[1].astro
+    if (!weatherResponse.ok) {
+      throw new Error('Failed to fetch weather data.')
+    }
+
+    const weatherData = await weatherResponse.json()
+    if (weatherData.forecast?.forecastday?.[1]) {
+      delete weatherData.forecast.forecastday[1].astro
+    }
 
     // Return the weather forecast data to the client
-    if (weatherData) {
-      const tomorrowWeather = weatherData.forecast?.forecastday[1]
+    const tomorrowWeather = weatherData.forecast?.forecastday[1]
 
-      return NextResponse.json(
-        { success: true, tomorrowWeather },
-        { status: 200 }
-      )
-    } else {
-      return NextResponse.json(
-        { success: false, error: 'Weather data not found.' },
-        { status: 404 }
-      )
-    }
+    return NextResponse.json(
+      { success: true, tomorrowWeather },
+      { status: 200 }
+    )
   } catch (error) {
     console.error('Failed to fetch weather information:', error)
     return NextResponse.json(
